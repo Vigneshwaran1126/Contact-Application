9982628648eaf4fd1aedfe919c0b99dd91e6fcdf
K Vicky <kvicky1200@gmail.com>
2021-06-01 11:48:37 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header=() => {
+    return(
+        <div className="ui fixed menu">
+            <div className="ui center container">
+                <h2>My Contacts</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
